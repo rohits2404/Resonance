8da6cb55de19a562860b25f6f18f3963574a8fb7
Rohit Sharma <rohits8731@gmail.com>
2026-07-28 12:32:58 +0530

    02: Dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const inter = Inter({
     variable: "--font-inter",
@@ -33,7 +34,9 @@ export default function RootLayout({
                 <body
                 className={`${inter.variable} ${geistMono.variable} antialiased`}
                 >
-                    {children}
+                    <TooltipProvider>
+                        {children}
+                    </TooltipProvider>
                     <Toaster/>
                 </body>
             </html>
